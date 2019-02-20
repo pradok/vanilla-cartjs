3828684ee5ce3ab9d44ff5df716b8467155c2b81
Pradosh Sinha <pradoshks@outlook.com>
2019-02-20 13:02:16 +1100

    feature #3: Refactor Product

--- src/products/Product.js
+++ src/products/Product.js
@@ -1,13 +1,15 @@
-
-
 class Product {
-    constructor(productData) {
-        this.productCode = '';
-        this.name = '';
-        this.price = 0;
-        this.priceDiscount = 0;
-        this.quantity = 0;
+  constructor(productData) {
+    const {productCode, name, price, priceDiscount, quantity} = productData;
+    if (!productCode || !name) {
+      throw new Error('Cannot create instance of Product, missing values in productData: productCode and name');
     }
+    this.productCode = productCode;
+    this.name = name;
+    this.price = price || 0;
+    this.priceDiscount = priceDiscount || 0;
+    this.quantity = quantity || 1;
+  }
 }
 
 module.exports = Product;
--- src/products/__tests__/Product.test.js
+++ src/products/__tests__/Product.test.js
@@ -2,15 +2,20 @@ const Product = require('../Product');
 
 describe ('Product class', () => {
     let productBase;
+    const productData = {
+        productCode: 'CHAIR_RED',
+        name: 'Red plastic chair',
+        price: 20
+    };
     beforeEach(() => {
-        productBase = new Product
+        productBase = new Product(productData);
     });
-    it('Product class is defined', () => {
+    it('should set the correct instance', () => {
         expect(productBase).toBeDefined();
-        expect(productBase.productCode).toBeDefined();
-        expect(productBase.name).toBeDefined();
-        expect(productBase.price).toBeDefined();
-        expect(productBase.priceDiscount).toBeDefined();
-        expect(productBase.quantity).toBeDefined();
+        expect(productBase.productCode).toEqual('CHAIR_RED');
+        expect(productBase.name).toEqual('Red plastic chair');
+        expect(productBase.price).toEqual(20);
+        expect(productBase.priceDiscount).toEqual(0);
+        expect(productBase.quantity).toEqual(1);
     });
 });
